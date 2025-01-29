0ca79444cd7b1c609b50b639634b82ce93c1d03d
LiDoY19 <lironlk19@gmail.com>
2025-01-29 16:10:28 +0200

    new changes terraform file

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,14 @@
 output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
+  description = "Public IP of the created EC2 instance"
   value       = aws_instance.flask_ec2.public_ip
 }
 
 output "ec2_instance_id" {
   description = "EC2 instance ID"
   value       = aws_instance.flask_ec2.id
+}
+
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.project_bucket.bucket
 }
