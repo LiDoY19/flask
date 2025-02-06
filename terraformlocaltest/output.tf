c97b6d828036fdbb0a54641c43232bd7ec6472fe
LiDoY19 <lironlk19@gmail.com>
2025-02-06 17:39:41 +0200

    new try

--- terraformlocaltest/output.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_public_ip" {
-  description = "Public IP of the created EC2 instance"
-  value = aws_instance.flask_ec2.public_ip
-}
-
-output "ec2_instance_id" {
-  description = "EC2 instance ID"
-  value = aws_instance.flask_ec2.id
-}
