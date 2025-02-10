e542f778788bad747bb9c079a4cd50af73e89a7b
LiDoY19 <lironlk19@gmail.com>
2025-02-10 15:07:20 +0200

    create a new cluster file by terraform

--- terraform/gkemain.tf
+++ terraform/gkemain.tf
@@ -4,32 +4,35 @@ provider "google" {
   region      = "us-central1"
 }
 
-data "google_container_cluster" "existing" {
+resource "google_container_cluster" "primary" {
   name     = "flask-app-cluster"
   location = "us-central1"
+
+  remove_default_node_pool = true
+  initial_node_count = 1
 }
 
-#resource "google_container_node_pool" "primary_nodes" {
-#  name       = "primary-node-pool"
-#  cluster    = data.google_container_cluster.existing.name
-#  location   = data.google_container_cluster.existing.location
-#
-#  node_count = 1
-#
-#  autoscaling {
-#    min_node_count = 1
-#    max_node_count = 2
-#  }
-#
-#  node_config {
-#    preemptible  = false
-#    machine_type = "e2-medium"
-#    disk_size_gb = 20
-#    oauth_scopes = [
-#      "https://www.googleapis.com/auth/cloud-platform"
-#    ]
-#  }
-#}
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+
+  node_count = 1
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 2
+  }
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-medium"
+    disk_size_gb = 20
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
 
 provider "kubernetes" {
   config_path = "~/.kube/config"
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,24 +1,24 @@
 output "gke_cluster_name" {
   description = "The name of the created GKE cluster"
-  value       = data.google_container_cluster.existing.name
+  value = google_container_cluster.primary.name
 }
 
 output "gke_cluster_endpoint" {
   description = "The endpoint for accessing the GKE cluster"
-  value       = data.google_container_cluster.existing.endpoint
+  value = google_container_cluster.primary.endpoint
 }
 
 output "gke_cluster_master_version" {
   description = "The Kubernetes version running on the GKE cluster"
-  value       = data.google_container_cluster.existing.master_version
+  value = google_container_cluster.primary.master_version
 }
 
 output "gcp_project_id" {
   description = "The Google Cloud project ID"
-  value       = data.google_container_cluster.existing.project
+  value = google_container_cluster.primary.project
 }
 
 output "gke_node_pool_instance_group" {
   description = "Instance group URLs for the default node pool"
-  value       = try(google_container_node_pool.primary_nodes.instance_group_urls, null)
+  value = try(google_container_node_pool.primary_nodes.instance_group_urls, null)
 }
