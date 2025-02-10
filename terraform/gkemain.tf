22499876da6017f15fbcd3bd234b76dd01754fa7
LiDoY19 <lironlk19@gmail.com>
2025-02-10 15:49:01 +0200

    remove the load_config from the main file

--- terraform/gkemain.tf
+++ terraform/gkemain.tf
@@ -38,7 +38,6 @@ provider "kubernetes" {
   host = google_container_cluster.primary.endpoint
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   token = data.google_client_config.default.access_token
-  load_config_file = false
 }
 
 data "google_client_config" "default" {}
