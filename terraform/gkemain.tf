652649f9f34e9b93ea39237173ce7a1fe62d9481
LiDoY19 <lironlk19@gmail.com>
2025-02-12 15:58:43 +0200

    edit the host in kubernetes block

--- terraform/gkemain.tf
+++ terraform/gkemain.tf
@@ -35,7 +35,7 @@ resource "google_container_node_pool" "primary_nodes" {
 }
 
 provider "kubernetes" {
-  host = "https://${google_container_cluster.primary.endpoint}
+  host = google_container_cluster.primary.endpoint
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   token = data.google_client_config.default.access_token
 }
